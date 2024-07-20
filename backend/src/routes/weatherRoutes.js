85fed4fb306e4686790f6b17d8b8aac4c5ba3f50
Thuận Phát <phat06112003@gmail.com>
2024-07-20 21:50:39 +0700

    add weather route

--- backend/src/routes/weatherRoutes.js
+++ backend/src/routes/weatherRoutes.js
@@ -2,8 +2,22 @@ const express = require('express');
 const router = express.Router();
 require('dotenv').config();
 
+const { getData } = require('../services/weatherService');
+
+//Route to get weather data of a location
 router.get('/get-data', async (req, res) => {
-  res.status(200).json({'status': 'ok'});
+  const { location } = req.query;
+
+  if (!location) {
+    return res.status(400).json({success: false, error: 'Location is missing.'});
+  }
+
+  const result = await getData(location);
+  if (result.success) {
+    res.status(200).json({...result});
+  } else {
+    res.status(400).json({...result});
+  }
 });
 
 module.exports = router;
