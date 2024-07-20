7f8a58df6f31b225bb0eaecc0a5ceace1eae6de2
Thuận Phát <phat06112003@gmail.com>
2024-07-20 14:40:52 +0700

    add email subscription section

--- frontend/src/pages/MainPage/MainPage.js
+++ frontend/src/pages/MainPage/MainPage.js
@@ -1,5 +1,5 @@
 import classNames from 'classnames/bind';
-import { FaMagnifyingGlass } from "react-icons/fa6";
+import { FaMagnifyingGlass, FaEnvelope } from "react-icons/fa6";
 import { FaHistory } from "react-icons/fa";
 import styles from './MainPage.module.scss';
 
@@ -78,6 +78,20 @@ function MainPage() {
               </li>
             </ul>
             <button className={cx('four-day-more')}>Load more</button>
+          </div>
+
+          <div className={cx('subscription')}>
+            <div className={cx('subscription-img')}>
+              <FaEnvelope />
+            </div>
+            <div className={cx('subscription-content')}>
+              <h3 className={cx('subscription-title')}>Receive weather forecast information via email</h3>
+              <p className={cx('subscription-text')}>Subscribe to receive daily weather forecast information directly in your inbox. Our service will keep you informed about the upcoming weather conditions, so you’re always prepared for what’s ahead. Sign up today and never miss an important weather update!</p>
+              <form className={cx('subscription-form')}>
+                <input type='email' className={cx('subscription-input')} placeholder='Enter your email here'/>
+                <button className={cx('subscription-btn')}>Notify Me</button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
