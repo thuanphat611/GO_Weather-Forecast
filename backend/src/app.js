cae02899912add4a6ff1b33864c6bc182881c4e3
Thuận Phát <phat06112003@gmail.com>
2024-07-22 23:37:51 +0700

    update backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,14 @@ const app = express();
 
 app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
-app.use(cors()); // Enable CORS for frontend
+// Enable CORS for frontend
+const corsOptions = {
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
 app.use('/assets', express.static(path.join(__dirname, '../public')));// serves static files
 
 app.use('/weather', weatherRouter);
