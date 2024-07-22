72e97881a428be58349ad79df18c770e4fdb0a39
Thuận Phát <phat06112003@gmail.com>
2024-07-22 19:07:08 +0700

    prepare to deploy

--- frontend/src/api/weatherApi.js
+++ frontend/src/api/weatherApi.js
@@ -4,6 +4,7 @@ const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 const getWeatherData = async (location) => {
   const url = `${backendUrl}/weather/get-data?location=${location}`;
+  console.log(url);
   const result = await axios.get(url);
   
   return result;
